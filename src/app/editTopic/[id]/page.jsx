e033eb2dd42f7a313a9b098670f5d23d6dd07384
vkhydras <victorkimaru8@gmail.com>
2024-02-05 19:37:06 +0300

    UserInterface

---
+++ src/app/editTopic/[id]/page.jsx
@@ -0,0 +1,10 @@
+import EditTopicForm from '@/components/EditTopicForm'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <EditTopicForm />
+    </div>
+  )
+}
---
+++ src/components/TopicList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import RemoveBtn from './RemoveBtn'
+import Link from 'next/link'
+import {HiPencilAlt} from 'react-icons/hi'
+
+export default function TopicList() {
+  return (
+    <>
+    <div className='p-4 border items-start border-slate-300 my-3 flex justify-between gap-5'>
+      <div>
+        <h2 className='font-bold text-2xl'>Topic Title</h2>
+        <div>Topic Description</div>
+      </div>
+
+      <div className='flex gap-2'>
+        <RemoveBtn />
+        <Link href='/editTopic/123'>
+            <HiPencilAlt size={24}/>
+        </Link>
+      </div>
+    </div>
+    </>
+  )
+}
